1f00b35929e2bbba5b9ae59188ab829248c37d15
jabbarrr7 <salamuljabbar1005@gmail.com>
2023-10-10 05:41:10 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
 
 client = MongoClient('mongodb+srv://salamull1005:jabbar1005@cluster0.t8jsdpg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp')
 db = client.dbsparta
